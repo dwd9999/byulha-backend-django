f9da54ed2631a98a9a93541218f7858f8baa89fa
dwd9999 <ojw1010@gmail.com>
2023-12-08 14:47:35 +0900

    Refactor: View 코드 개선

--- byulha_backend_django/classifier/views.py
+++ byulha_backend_django/classifier/views.py
@@ -16,22 +16,23 @@ def post(request):
         # S3 접근
         s3_client = boto3.client('s3', aws_access_key_id=S3_ACCESS_KEY, aws_secret_access_key=S3_SECRET_KEY)
         response = s3_client.get_object(Bucket=S3_BUCKET_NAME, Key='/' + request_json['fileId'])
-        img_model(response['Body'])
 
         # 모델
-        category_name = 'SPORTY:50,asdf:49.7,werscv:30'
+        category_percent = img_model(response['Body'])
+        # category_name = 'SPORTY:50,asdf:49.7,werscv:30'
 
         # 데이터 json 형식 변환
         data_to_send = {
             'nickname': request_json['nickname'],
             'fileId': request_json['fileId'],
-            'category_percent': category_name,
+            'category_percent': category_percent,
         }
         print(data_to_send)
 
         # 데이터 반환
         return JsonResponse(data_to_send)
 
+    # 오류 발생 시 Response 500
     except Exception as e:
         print(f'error: {e}')
         return JsonResponse({"error": str(e)}, status=500)
